1d8f8ae2ba5e1e4dd297e43d86c2d0e839e52b0a
Vasilios Tsapalos <littleprog@gmail.com>
2018-04-13 11:07:56 +0300

    Server service for steering system

---
+++ src/main/kotlin/car/server/SteeringSystem.kt
@@ -0,0 +1,36 @@
+package car.server
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class SteeringSystem{
+
+    /*
+        All possible combinations and their meaning:
+        direction = 1 & value = x (x>0) -> Turn right with x tension
+        direction = -1 & value = x (x>0) -> Turn left with x tension
+        direction = 0 & value = 0 -> Set steering to straight
+     */
+    @GetMapping("/steering_system")
+    fun setThrottle(@RequestParam(value = "direction", defaultValue = "DIRECTION_STRAIGHT") direction: Int,
+                    @RequestParam(value = "value", defaultValue =  "0") value: Int): String
+    {
+
+        //TODO add function for the hardware
+        synchronized(this){
+            Thread.sleep(1000)
+            println("Steering $direction with $value tension")
+        }
+
+        return "Steering $direction with $value tension"
+    }
+
+    companion object {
+        const val DIRECTION_RIGHT = 1
+        const val DIRECTION_LEFT = -1
+        const val DIRECTION_STRAIGHT = 0
+    }
+
+}
