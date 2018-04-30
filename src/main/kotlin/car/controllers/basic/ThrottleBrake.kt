c914ad8bd5254377fa84ced9eeca266d1cf86926
Vasilios Tsapalos <littleprog@gmail.com>
2018-04-30 11:50:13 +0300

    Declare common use motor function

--- src/main/kotlin/car/controllers/basic/ThrottleBrake.kt
+++ src/main/kotlin/car/controllers/basic/ThrottleBrake.kt
@@ -10,6 +10,13 @@ interface ThrottleBrake {
     fun parkingBrake(value: Int): String
     fun handbrake(value: Int): String
 
+    fun applyPwmValues(motorFRpin00: Int = 0, motorFRpin01: Int = 0,
+                                 motorFLpin00: Int = 0, motorFLpin01: Int = 0,
+                                 motorRRpin00: Int = 0, motorRRpin01: Int = 0,
+                                 motorRLpin00: Int = 0, motorRLpin01: Int = 0){
+        //TODO implement it here
+    }
+
     fun reset() {
         action = ThrottleBrakeSystem.ACTION_STILL
         value = 0
