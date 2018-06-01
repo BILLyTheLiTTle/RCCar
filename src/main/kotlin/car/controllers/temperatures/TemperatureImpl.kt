b5048cd4928c48bd9ec5daeef80c8718ae9849ae
Vasilios Tsapalos <littleprog@gmail.com>
2018-06-01 12:48:11 +0300

    Improve class inheritance

---
+++ src/main/kotlin/car/controllers/temperatures/TemperatureImpl.kt
@@ -0,0 +1,27 @@
+package car.controllers.temperatures
+
+import car.server.EngineSystem
+
+open class TemperatureImpl: Temperature {
+
+    open val ID = "motor_front_right_temp"
+    protected open val MIN_MEDIUM_TEMP = 1
+    protected open val MAX_MEDIUM_TEMP = 2
+
+    override val value: Int
+        get() {
+            // TODO read the appropriate sensor
+            val temp = readSensor(ID)
+
+            warning = when {
+                temp < MIN_MEDIUM_TEMP -> WARNING_TYPE_NORMAL
+                temp > MAX_MEDIUM_TEMP -> WARNING_TYPE_HIGH
+                else -> WARNING_TYPE_MEDIUM
+            }
+
+            return temp
+        }
+
+    override var warning = EngineSystem.EMPTY_STRING
+        protected set
+}
