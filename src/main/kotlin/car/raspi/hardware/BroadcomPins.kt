bc4ef9cb9ba27c7c38fe1811fe24e3dad6d29858
Vasilios Tsapalos <littleprog@gmail.com>
2018-07-24 10:25:18 +0300

    Add BCM pins' names

---
+++ src/main/kotlin/car/raspi/hardware/BroadcomPins.kt
@@ -0,0 +1,83 @@
+package car.raspi.hardware
+
+import com.pi4j.io.gpio.Pin
+import com.pi4j.io.gpio.RaspiPin
+
+val RaspiPin.BCM_02: Pin
+    get() = RaspiPin.GPIO_08
+
+val RaspiPin.BCM_03: Pin
+    get() = RaspiPin.GPIO_09
+
+val RaspiPin.BCM_04: Pin
+    get() = RaspiPin.GPIO_07
+
+val RaspiPin.BCM_05: Pin
+    get() = RaspiPin.GPIO_21
+
+val RaspiPin.BCM_06: Pin
+    get() = RaspiPin.GPIO_22
+
+val RaspiPin.BCM_07: Pin
+    get() = RaspiPin.GPIO_11
+
+val RaspiPin.BCM_08: Pin
+    get() = RaspiPin.GPIO_10
+
+val RaspiPin.BCM_09: Pin
+    get() = RaspiPin.GPIO_13
+
+val RaspiPin.BCM_10: Pin
+    get() = RaspiPin.GPIO_12
+
+val RaspiPin.BCM_11: Pin
+    get() = RaspiPin.GPIO_14
+
+val RaspiPin.BCM_12: Pin
+    get() = RaspiPin.GPIO_26
+
+val RaspiPin.BCM_13: Pin
+    get() = RaspiPin.GPIO_23
+
+val RaspiPin.BCM_14: Pin
+    get() = RaspiPin.GPIO_15
+
+val RaspiPin.BCM_15: Pin
+    get() = RaspiPin.GPIO_16
+
+val RaspiPin.BCM_16: Pin
+    get() = RaspiPin.GPIO_27
+
+val RaspiPin.BCM_17: Pin
+    get() = RaspiPin.GPIO_00
+
+val RaspiPin.BCM_18: Pin
+    get() = RaspiPin.GPIO_01
+
+val RaspiPin.BCM_19: Pin
+    get() = RaspiPin.GPIO_24
+
+val RaspiPin.BCM_20: Pin
+    get() = RaspiPin.GPIO_28
+
+val RaspiPin.BCM_21: Pin
+    get() = RaspiPin.GPIO_29
+
+val RaspiPin.BCM_22: Pin
+    get() = RaspiPin.GPIO_03
+
+val RaspiPin.BCM_23: Pin
+    get() = RaspiPin.GPIO_04
+
+val RaspiPin.BCM_24: Pin
+    get() = RaspiPin.GPIO_05
+
+val RaspiPin.BCM_25: Pin
+    get() = RaspiPin.GPIO_06
+
+val RaspiPin.BCM_26: Pin
+    get() = RaspiPin.GPIO_25
+
+val RaspiPin.BCM_27: Pin
+    get() = RaspiPin.GPIO_02
+
