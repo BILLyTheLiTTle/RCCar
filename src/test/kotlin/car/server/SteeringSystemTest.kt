1d4c6533c34eac509ffdd31ec477a4fe05a58722
Vasilios Tsapalos <littleprog@gmail.com>
2018-08-02 10:25:33 +0300

    More tests for the SteeringSystem

--- src/test/kotlin/car/server/SteeringSystemTest.kt
+++ src/test/kotlin/car/server/SteeringSystemTest.kt
@@ -18,17 +18,38 @@ internal class SteeringSystemTest(@Autowired val restTemplate: TestRestTemplate)
 
     // setSteeringAction
     @Test
-    fun `turn steering`() {
+    fun `turn steering to nothing is successful`() {
         val entity = restTemplate.getForEntity<String>("/set_steering_system")
         assertThat(entity.statusCode).isEqualTo(HttpStatus.OK)
         assertThat(entity.body).isEqualTo(EngineSystem.SUCCESS)
+    }
+    @Test
+    fun `turn steering to unknown direction should be an error`() {
+        val entity = restTemplate.getForEntity<String>("/set_steering_system?direction=oops")
+        assertThat(entity.statusCode).isEqualTo(HttpStatus.OK)
+        assertThat(entity.body).contains("ERROR: Entered in else condition")
     }
 
     // getSteeringDirection
     @Test
     fun `steering direction when no steering action happened is straight`() {
+        val entityAction = restTemplate.getForEntity<String>("/set_steering_system")
         val entity = restTemplate.getForEntity<String>("/get_steering_direction")
         assertThat(entity.statusCode).isEqualTo(HttpStatus.OK)
-        assertThat(entity.body).isEqualTo(SteeringImpl.direction)
+        assertThat(entity.body).isEqualTo(SteeringImpl.direction).isEqualTo(SteeringSystem.ACTION_STRAIGHT)
+    }
+    @Test
+    fun `steering direction should be left`() {
+        val entityAction = restTemplate.getForEntity<String>("/set_steering_system?direction=left")
+        val entity = restTemplate.getForEntity<String>("/get_steering_direction")
+        assertThat(entity.statusCode).isEqualTo(HttpStatus.OK)
+        assertThat(entity.body).isEqualTo(SteeringImpl.direction).isEqualTo(SteeringSystem.ACTION_TURN_LEFT)
+    }
+    @Test
+    fun `steering direction should be right`() {
+        val entityAction = restTemplate.getForEntity<String>("/set_steering_system?direction=right")
+        val entity = restTemplate.getForEntity<String>("/get_steering_direction")
+        assertThat(entity.statusCode).isEqualTo(HttpStatus.OK)
+        assertThat(entity.body).isEqualTo(SteeringImpl.direction).isEqualTo(SteeringSystem.ACTION_TURN_RIGHT)
     }
 }
