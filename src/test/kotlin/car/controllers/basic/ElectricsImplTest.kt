8a0a1e36bd76c7e7a048d3f08ca7d9177b79e276
BILLyTheLiTTle <littleprog@gmail.com>
2018-08-14 19:27:00 +0300

    Design tests for ElectricsImpl

--- src/test/kotlin/car/controllers/basic/ElectricsImplTest.kt
+++ src/test/kotlin/car/controllers/basic/ElectricsImplTest.kt
@@ -13,8 +13,101 @@ import org.springframework.test.context.junit.jupiter.SpringExtension
 internal class ElectricsImplTest {
 
 
+    // positionLightsState
     @Test
-    fun `method name`() {
+    fun `turn on position lights`() {
+
+    }
+    @Test
+    fun `turn off position lights`() {
+
+    }
+
+    // drivingLightsState
+    @Test
+    fun `turn on driving lights`() {
+
+    }
+    @Test
+    fun `turn off driving lights`() {
+
+    }
+
+    // longRangeLightsState
+    @Test
+    fun `turn on long range lights`() {
+
+    }
+    @Test
+    fun `turn off long range lights`() {
+
+    }
+
+    // brakingLightsState
+    @Test
+    fun `turn on braking lights`() {
+
+    }
+    @Test
+    fun `turn off braking lights`() {
+
+    }
+
+    // reverseLightsState
+    @Test
+    fun `turn on reverse lights`() {
+
+    }
+    @Test
+    fun `turn off reverse lights`() {
+
+    }
+
+    // leftTurnLightsState
+    @Test
+    fun `turn on left turn lights`() {
+
+    }
+    @Test
+    fun `turn off left turn lights`() {
+
+    }
+
+    // rightTurnLightsState
+    @Test
+    fun `turn on right turn lights`() {
+
+    }
+    @Test
+    fun `turn off right turn lights`() {
+
+    }
+
+    // emergencyLightsState
+    @Test
+    fun `turn on emergency lights`() {
+
+    }
+    @Test
+    fun `turn off emergency lights`() {
+
+    }
+
+    // doHeadlightsSignal
+    @Test
+    fun `do headlights signal`() {
+
+    }
+
+    // handleLeds
+    @Test
+    fun `turn on or off the LEDs`() {
+        // TODO needs hardware to test it, probably no need to test it and get assertions
+    }
+
+
+    @Test
+    fun reset() {
 
     }
 }
