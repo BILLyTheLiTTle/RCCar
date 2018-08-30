f7fa466fc763847ee9502b8155a1838e9dd91442
BILLyTheLiTTle <littleprog@gmail.com>
2018-08-30 16:44:16 +0300

    Improve kotlin reflection usage for SteeringImplTest

--- src/test/kotlin/car/controllers/basic/SteeringImplTest.kt
+++ src/test/kotlin/car/controllers/basic/SteeringImplTest.kt
@@ -6,26 +6,13 @@ import org.assertj.core.api.Assertions.*
 import org.junit.jupiter.api.Test
 
 import org.junit.jupiter.api.Assertions.*
-import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.junit.jupiter.SpringExtension
-import java.lang.reflect.Field
 
 @SpringBootTest
 @ExtendWith(SpringExtension::class)
 internal class SteeringImplTest {
-
-    var direction: Field? = null
-    var value: Field? = null
-
-    @BeforeEach
-    internal fun setUp() {
-        direction = SteeringImpl::class.java.getDeclaredField("direction")
-        direction?.isAccessible = true
-        value = SteeringImpl::class.java.getDeclaredField("value")
-        value?.isAccessible = true
-    }
 
     // turn
     @Test
@@ -34,10 +21,10 @@ internal class SteeringImplTest {
             SteeringSystem.STEERING_VALUE_40)
         assertThat(ret).isEqualTo(EngineSystem.SUCCESS)
         assertThat(SteeringImpl.direction)
-            .isEqualTo(direction?.get(SteeringImpl).toString())
+            .isEqualTo(SteeringImpl::direction.get())
             .isEqualTo(SteeringSystem.ACTION_TURN_RIGHT)
             .isEqualTo("right")
-        assertThat(value?.getInt(ThrottleBrakeImpl))
+        assertThat(SteeringImpl::value.get())
             .isEqualTo(SteeringSystem.STEERING_VALUE_40)
             .isEqualTo(40)
     }
@@ -47,10 +34,10 @@ internal class SteeringImplTest {
             SteeringSystem.STEERING_VALUE_60)
         assertThat(ret).isEqualTo(EngineSystem.SUCCESS)
         assertThat(SteeringImpl.direction)
-            .isEqualTo(direction?.get(SteeringImpl).toString())
+            .isEqualTo(SteeringImpl::direction.get())
             .isEqualTo(SteeringSystem.ACTION_TURN_LEFT)
             .isEqualTo("left")
-        assertThat(value?.getInt(ThrottleBrakeImpl))
+        assertThat(SteeringImpl::value.get())
             .isEqualTo(SteeringSystem.STEERING_VALUE_60)
             .isEqualTo(60)
     }
@@ -59,10 +46,10 @@ internal class SteeringImplTest {
         val ret = SteeringImpl.turn(SteeringSystem.ACTION_STRAIGHT)
         assertThat(ret).isEqualTo(EngineSystem.SUCCESS)
         assertThat(SteeringImpl.direction)
-            .isEqualTo(direction?.get(SteeringImpl).toString())
+            .isEqualTo(SteeringImpl::direction.get())
             .isEqualTo(SteeringSystem.ACTION_STRAIGHT)
             .isEqualTo("straight")
-        assertThat(value?.getInt(ThrottleBrakeImpl))
+        assertThat(SteeringImpl::value.get())
             .isEqualTo(SteeringSystem.STEERING_VALUE_00)
             .isEqualTo(0)
     }
@@ -72,10 +59,10 @@ internal class SteeringImplTest {
     fun reset() {
         SteeringImpl.reset()
         assertThat(SteeringImpl.direction)
-            .isEqualTo(direction?.get(SteeringImpl).toString())
+            .isEqualTo(SteeringImpl::direction.get())
             .isEqualTo(SteeringSystem.ACTION_STRAIGHT)
             .isEqualTo("straight")
-        assertThat(value?.getInt(ThrottleBrakeImpl))
+        assertThat(SteeringImpl::value.get())
             .isEqualTo(SteeringSystem.STEERING_VALUE_00)
             .isEqualTo(0)
     }
